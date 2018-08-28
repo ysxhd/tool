e98dd01c33a6fbd1c95f6b7528fa0fbf16bf94e8
ysxhd <1109342967>
2018-08-28 13:36:23 +0800

    ant

---
+++ react-插件/ant-按需加载.jsx
@@ -0,0 +1,15 @@
+"babel": {
+    "presets": [
+        "react-app"
+    ],
+    "plugins": [
+        "transform-decorators-legacy",
+        [
+            "import",
+            {
+                "libraryName": "antd",
+                "style": "css"
+            }
+        ]
+    ]
+}
