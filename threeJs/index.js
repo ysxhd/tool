7046965f1cf0b9c3d7c15eb01fb240ca2a00f913
zhangning <1109342967@qq.com>
2019-12-03 18:57:32 +0800

    完善所有的线”

--- threeJs/index.js
+++ threeJs/index.js
@@ -7,6 +7,7 @@ class Disk3D {
       this.cilcleNum = cilcleNum; // cicleNum是圆形平面类型数量
       this.middleRadius = 400; // 最大圆半径,小球环绕的最外层看不到的圆
       this.cicleRadius = 340; // 饼图圆
+      this.defaultAngle = 0; // 每个环绕小球的夹角
       this.width = 0;  // 场景宽
       this.height = 0; // 场景高
       this.renderer = null; // 渲染器
@@ -19,9 +20,11 @@ class Disk3D {
       this.centerPoint = { x: 0, y: -330, z: 0 }; // 底部正中间小球中心点
       this.colors = ['#c5974a', '#bc5a5e', '#4ca96f' ]; 
       this.hasPosData = []; // 最外层小球的位置
+      this.textGroup = new THREE.Object3D(); // 文字组合
       this.lineGroup = new THREE.Object3D(); // 线段组合
       this.pointGroup = new THREE.Object3D(); // 旋转组合
       this.pieGroup = new THREE.Object3D(); // 原形的饼状组合
+      this.originAngle = 0; // 分隔竖线的角度
       this.clock = null;
       this.orbitControl = null;
     }
@@ -61,8 +64,8 @@ class Disk3D {
         this.camera.position.y = this.cameraY; // -140
         this.camera.position.z = this.cameraZ; // 300
         // 坐标轴调试用的
-        let axes = new THREE.AxisHelper(280);
-        this.scene.add(axes);
+        // let axes = new THREE.AxisHelper(280);
+        // this.scene.add(axes);
     }
     /**
      * 绘制光
@@ -118,13 +121,12 @@ class Disk3D {
     getPosPoint(){
         // 求出每个小球之间的角度
         let splitAngle = +(360 / this.number).toFixed(2);
-        let defaultAngle = splitAngle;
+        this.defaultAngle = splitAngle;
         this.data.map((val, i) => {
              val.map((item) => {
-                splitAngle += defaultAngle;
-                let { x, y, z } = this.italicLineGenerate(splitAngle);
-                this.hasPosData.push({ name: item, x: x, y, z, color: this.colors[i]});
-                // this.hasPosBallData.push({ name: item, x, y, z, color: this.colors[i]}); // 小球的位置
+                splitAngle += this.defaultAngle;
+                let { x, y, z } = this.italicLineGenerate(splitAngle, false, this.middleRadius);
+                this.hasPosData.push({ name: item, x, y, z, color: this.colors[i]});
              })
         })
         // 连线小球与底部中心点小球
@@ -157,23 +159,119 @@ class Disk3D {
             sphere.position.y = eachPoint.y;
             sphere.position.z = eachPoint.z;
             this.pointGroup.add(sphere);
+            // 绘制文字
+            this.drawText(eachPoint, i);
         }
         this.scene.add(this.lineGroup);
         this.scene.add(this.pointGroup);
+        // this.scene.add(this.textGroup);
+    }
+    /**
+     * 
+     * @param {*} eachPoint 文字坐标轴位置，名字
+     */
+    drawText(eachPoint, i){
+        let canvasWidth = 256;
+        let canvasHeight = 128;
+        let textFontsize = 0;
+        let scale = null;
+        let name = eachPoint.name;
+        let nameLenght = name.length;
+        let locationX = eachPoint.x;
+        let locationY = eachPoint.y;
+        let locationZ = eachPoint.z;
+        if(nameLenght === 2){
+            textFontsize = 125;
+            let x = 0.28;
+            scale = {x: x, y: x / 2, z: 1.25 * x};
+            locationX = locationX > 0 ? (locationX - 6) : (locationX + 6);
+            locationZ = locationZ > 0 ? (locationZ - 6) : (locationZ + 6);
+        }else if(nameLenght === 3){
+            textFontsize = 85;
+            let x = 0.6;
+            scale = {x: x, y: x / 2, z: 1.25 * x};
+        }else if(nameLenght === 4){
+            textFontsize = 65;
+            let x = 1;
+            scale = {x: x, y: x / 2, z: 1.25 * x};
+            locationX = locationX > 0 ? (locationX + 8) : (locationX - 8);
+            locationZ = locationZ > 0 ? (locationZ + 8) : (locationZ - 8);
+        }else if(nameLenght === 5){
+            textFontsize = 51;
+            let x = 1.50;
+            scale = {x: x, y: x / 2, z: 1.25 * x};
+            locationX = locationX > 0 ? (locationX + 22) : (locationX - 22);
+            locationZ = locationZ > 0 ? (locationZ + 15) : (locationZ - 15);
+        }else if(nameLenght === 6){
+            textFontsize = 40;
+            let x = 2.6;
+            scale = {x: x, y: x / 2, z: 1.25 * x};
+            locationX = locationX > 0 ? (locationX + 22) : (locationX - 22);
+            locationZ = locationZ > 0 ? (locationZ + 15) : (locationZ - 15);
+        }
+        let radian = this.defaultAngle * i;
+        // 创建文字
+        let rotation = Math.PI * 3 / 2 + radian;
+        let spriteMaterialColor = eachPoint.color;
+
+        var text = this.makeTextSprite(name, scale, spriteMaterialColor, { fontsize: textFontsize, width: canvasWidth, height: canvasHeight}, rotation);
+        console.log(text);
+        text.position.set(locationX, locationY, locationZ);
+        this.textGroup.add(text);
+    }
+        /**
+     * 创建永远面向相机的2D文字
+     * */
+    makeTextSprite(message, scale, spriteMaterialColor, parameters, rotation) {
+        if (parameters === undefined) {
+            parameters = {};
+        }
+        var fontface = parameters.hasOwnProperty('fontface') ? parameters['fontface'] : 'Microsoft YaHei';
+        var fontsize = parameters.hasOwnProperty('fontsize') ? parameters['fontsize'] : 100;
+        var borderThickness = parameters.hasOwnProperty('borderThickness') ? parameters['borderThickness'] : 0;
+        var textColor = parameters.hasOwnProperty('textColor') ? parameters['textColor'] : { r: 255, g: 255, b: 255, a: 1.0 };
+        scale = scale ? scale : {x: 0.5, y: 0.25, z: 0.75};
+
+        var canvasWidth = parameters.hasOwnProperty('width') ? parameters['width'] : '512';
+        var canvasHeight = parameters.hasOwnProperty('height') ? parameters['height'] : '256';
+
+        var canvas = document.createElement('canvas');
+        canvas.width = canvasWidth;
+        canvas.height = canvasHeight;
+        var context = canvas.getContext('2d');
+        context.font = 'bold ' + fontsize + 'px ' + fontface;
+
+        context.lineWidth = borderThickness;
+        context.textAlign = 'left';
+        context.fillStyle = 'rgba(' + textColor.r + ', ' + textColor.g + ', ' + textColor.b + ', 1.0)'; 
+        context.fillText(message, borderThickness, fontsize + borderThickness);
+
+        var texture = new THREE.Texture(canvas);
+        texture.needsUpdate = true;
+
+        var spriteMaterial = new THREE.SpriteMaterial({ map: texture, rotation: rotation, color: '#ffffff'}); // color: '#ffffff'
+        spriteMaterial.color = spriteMaterialColor;
+
+        var sprite = new THREE.Sprite(spriteMaterial);
+        let spriteScale = 0.85;
+        sprite.scale.set(scale.x * fontsize * spriteScale, scale.y * fontsize * spriteScale, scale.z * fontsize * spriteScale);
+        // sprite.scale.set(2 *  20, 1 * 20, 1 * 20);
+        return sprite;
     }
     /**
      * 根据角度以及半径 ，求出当前点xy的坐标
      * @param {*} angle 角度
      * @param {*} onlyXy 是否包含z轴
+     * @param {*} radius 圆的半径
      */
-    italicLineGenerate(angle, onlyXy){
+    italicLineGenerate(angle, onlyXy, radius){
         if(onlyXy){
-            let x = +(Math.cos(angle * (Math.PI / 180)) * this.cicleRadius).toFixed(2);
-            let y = +(Math.sin(angle * (Math.PI / 180)) * this.cicleRadius).toFixed(2);
+            let x = +(Math.cos(angle * (Math.PI / 180)) * radius).toFixed(2);
+            let y = +(Math.sin(angle * (Math.PI / 180)) * radius).toFixed(2);
             return {x, y};
         }
-        let x = +(Math.cos(angle * (Math.PI / 180)) * this.middleRadius).toFixed(2);
-        let z = +(Math.sin(angle * (Math.PI / 180)) * this.middleRadius).toFixed(2);
+        let x = +(Math.cos(angle * (Math.PI / 180)) * radius).toFixed(2);
+        let z = +(Math.sin(angle * (Math.PI / 180)) * radius).toFixed(2);
         return {x, y: 0, z};
     }
     drawCilcle(){
@@ -191,8 +289,7 @@ class Disk3D {
             // 求出类型的分隔线
             this.pieGroup.add(this.splitLine(originAngle));
             // 求出最外圈竖线分割线
-            this.splitVerticalLine(originAngle, i);
-
+            this.splitVerticalLine(angle, i);
         })
         this.pieGroup.rotateX(-Math.PI / 2);
         this.scene.add(this.pieGroup);
@@ -203,35 +300,33 @@ class Disk3D {
     splitVerticalLine(angle, i){
         // 每个扇形区分出多少份，求出夹角
         let splitAngle = angle / this.cicleTwoData[i].length;
-        let originAngle = 0;
-        var geometry = new THREE.Geometry();
-        var material = new THREE.MeshBasicMaterial( { color: 0xffffff} );
-        this.cicleTwoData[i].map((val) => {
-            originAngle += splitAngle;
-            let transformAn = 180 / Math.PI * originAngle;
-            console.log(transformAn);
-            
-            let { x, y } = this.italicLineGenerate(transformAn, true);
-            geometry.vertices.push(
-                new THREE.Vector3( 0, 0, 0 ),
-                new THREE.Vector3( x, y, 0 )
-            );
-            var line = new THREE.Line(geometry, material);
-            this.pieGroup.add(line);
+        this.cicleTwoData[i].map(() => {
+            this.originAngle += splitAngle;
+            this.pieGroup.add(this.splitLine(this.originAngle, true));
         })
     }
     /**
      * 
      * @param {number} angle 角度
+     *  @param {number} isOriginalLine 是否是从圆点开始连接的
      */
-    splitLine(angle){
+    splitLine(angle, isOriginalLine){
         var geometry = new THREE.Geometry();
         let transformAn = 180 / Math.PI * angle;
-        let { x, y } = this.italicLineGenerate(transformAn, true);
-        geometry.vertices.push(
-            new THREE.Vector3( 0, 0, 0 ),
-            new THREE.Vector3( x, y, 0 )
-        );
+        if(isOriginalLine){
+            let point0 = this.italicLineGenerate(transformAn, true, 226);
+            let point1 = this.italicLineGenerate(transformAn, true, this.cicleRadius);
+            geometry.vertices.push(
+                new THREE.Vector3( point0.x, point0.y, 0 ),
+                new THREE.Vector3( point1.x, point1.y, 0 )
+            );
+        }else{
+            let { x, y } = this.italicLineGenerate(transformAn, true, this.cicleRadius);
+            geometry.vertices.push(
+                new THREE.Vector3( 0, 0, 0 ),
+                new THREE.Vector3( x, y, 0 )
+            );
+        }
         var material = new THREE.MeshBasicMaterial( { color: 0xffffff} );
         var line = new THREE.Line(geometry, material);
         return line;
@@ -269,7 +364,7 @@ class Disk3D {
         let animateDeg = 0.002;
         n = n + animateDeg;
         this.renderer.render(this.scene, this.camera);
-        // this.scene.rotation.y -= animateDeg;
+        this.scene.rotation.y -= animateDeg;
         requestAnimationFrame(this.animate.bind(this));
     }
 }
